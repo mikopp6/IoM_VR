ea7d0eda20238bcbfc9868488ff8d570ce02d4d3
Miko <miko.palojarvi@student.oulu.fi>
2022-11-11 14:25:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# IoM_VR
+
+## Isle of Man VR Experience
+
+University of Oulu project
+
+Mika Ihamäki
+Miko Palojärvi
---
+++ buildapk_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,69 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ColorController::Start()
+extern void ColorController_Start_mD96DE3EA712664A13DD35FD392CB8CAEA3B3A181 (void);
+// 0x00000002 System.Void ColorController::OnCollisionEnter(UnityEngine.Collision)
+extern void ColorController_OnCollisionEnter_m976A873491DCFF2E32DE49C457E011902882134C (void);
+// 0x00000003 System.Void ColorController::OnCollisionExit(UnityEngine.Collision)
+extern void ColorController_OnCollisionExit_mB87EA69A31347A820B7360BD7897CEFC12AD4EEA (void);
+// 0x00000004 System.Void ColorController::.ctor()
+extern void ColorController__ctor_mF92F9F78E028F386494B8D68B3B6DA5A57FA4758 (void);
+// 0x00000005 System.Void PlayerController::Start()
+extern void PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF (void);
+// 0x00000006 System.Void PlayerController::Update()
+extern void PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33 (void);
+// 0x00000007 System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33 (void);
+static Il2CppMethodPointer s_methodPointers[7] = 
+{
+	ColorController_Start_mD96DE3EA712664A13DD35FD392CB8CAEA3B3A181,
+	ColorController_OnCollisionEnter_m976A873491DCFF2E32DE49C457E011902882134C,
+	ColorController_OnCollisionExit_mB87EA69A31347A820B7360BD7897CEFC12AD4EEA,
+	ColorController__ctor_mF92F9F78E028F386494B8D68B3B6DA5A57FA4758,
+	PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF,
+	PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33,
+	PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33,
+};
+static const int32_t s_InvokerIndices[7] = 
+{
+	3463,
+	2823,
+	2823,
+	3463,
+	3463,
+	3463,
+	3463,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	7,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
